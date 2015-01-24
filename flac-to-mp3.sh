50044075e523860fd558f4d5925b7b328c62f285
Darren O'Connor <mellow.drifter@gmail.com>
2015-01-24 12:11:37 +0000

    updated

--- flac-to-mp3.sh
+++ flac-to-mp3.sh
@@ -7,6 +7,7 @@
 # This script, when run in a folder full of FLAC files, will create high quality VBR mp3's for use in mp3 players.
 # This version uses lame 3.98. If you use version 3.97 and below you'll need to change a few options
 
+#Requires both flac and lame installed. Use brew to install if on MacOS
 
 mkdir wav/
 flac -d *.flac
